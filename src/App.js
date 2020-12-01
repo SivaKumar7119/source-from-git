4429e88ac334d02075b6ae268a96485f84224bbb
Siva Kumar <sivakumar@travash.com>
2020-12-01 21:38:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Source From GITHUB  <code>src/App.js</code> and save to reload.
+          Source From GITHUB - Updated <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
